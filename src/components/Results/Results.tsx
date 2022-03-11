615e6867029cddaa659841efe8cf2f93d442639a
Fulvia Mourikis <zum281@gmail.com>
2022-03-11 20:29:09 +0100

    fix: fixed gross wpm calculation

--- src/components/Results/Results.tsx
+++ src/components/Results/Results.tsx
@@ -1,30 +1,26 @@
-import React, { useEffect, useMemo } from "react";
+import React, { useEffect, useMemo, useState } from "react";
 import { useGameContext } from "../../context/GameContext";
 import { CharColor } from "../../types/enums";
-import { timeInMinutes } from "../../utils/timeUtils";
+import { grossWPM, timeInMinutes } from "../../utils/timeUtils";
 
 const Results = () => {
 	const { chars, timer } = useGameContext();
+	const [wpm, setWpm] = useState(0);
 
 	const typedChars = useMemo(
 		() => chars.filter((char) => char.color !== CharColor.BLACK),
 		[chars]
 	);
 
-	const typedWords = useMemo(() => typedChars.length / 5, [chars]);
-
-	const GrossWPM = useMemo(() => {
-		const time = timeInMinutes(timer);
-		console.table({ timer, time });
-		if (time > 0) {
-			return typedWords / time;
-		} else return 0;
-	}, [typedWords, timer]);
+	useEffect(() => {
+		if (typedChars.length > 0)
+			setWpm(grossWPM(timeInMinutes(timer), typedChars.length));
+	}, [timer, typedChars]);
 
 	useEffect(() => {
 		console.log(typedChars);
 	}, [typedChars]);
-	return <p>WPM: {GrossWPM}</p>;
+	return <p>WPM: {wpm}</p>;
 };
 
 export default Results;
--- src/utils/timeUtils.ts
+++ src/utils/timeUtils.ts
@@ -1,2 +1,6 @@
-export const timeInMinutes = (seconds: number): number =>
-	Math.floor(seconds / 60);
+export const timeInMinutes = (seconds: number): number => seconds / 60;
+
+export const grossWPM = (minutes: number, typedChars: number): number => {
+	const typedWords = typedChars / 5;
+	return Math.floor(typedWords / minutes);
+};
