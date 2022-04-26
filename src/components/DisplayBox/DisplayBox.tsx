172937d77bbcbd54a6843761765b6e7122391b57
Fulvia Mourikis <zum281@gmail.com>
2022-04-27 00:57:49 +0200

    Added monospace font

--- src/components/DisplayBox/DisplayBox.tsx
+++ src/components/DisplayBox/DisplayBox.tsx
@@ -9,7 +9,7 @@ interface Props {
 
 const DisplayBox: FC<Props> = ({ chars }) => {
 	return (
-		<p>
+		<p style={{ fontFamily: "DM Mono, monospace" }}>
 			{chars.map((char, i) => {
 				return <Char key={i} char={char.value} color={char.color} />;
 			})}
